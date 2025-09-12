8c94062cea7dacd9b113b3ba08dc16a1fa843d60
maxtharp <maxwelltharp7@gmail.com>
2025-09-12 14:32:59 -0400

    Round Robin complete

--- src/RR.java
+++ src/RR.java
@@ -13,11 +13,57 @@ public class RR extends Algorithm{
     // Processes that have not yet arrived
     private final Queue<Process> processesToArrive;
 
-    public RR(List<Process> allProcesses){
+    private final int testCaseTime;
+    int now = 0;
+
+    public RR(List<Process> allProcesses, int testCaseTime){
         super(allProcesses);
         processesToArrive = new LinkedList<>(allProcesses);
+        this.testCaseTime = testCaseTime;
     }
 
     @Override
-    public void schedule(){}
+    public void schedule(){
+        System.out.println("Round Robin:");
+
+        while (!readyQueue.isEmpty() || !processesToArrive.isEmpty()) {
+            if (readyQueue.isEmpty()) {
+                Process process = processesToArrive.remove();
+                if (now < process.getArrivalTime()) {
+                    //advance the simulation clock to the next process's arrival time
+                    now = process.getArrivalTime();
+                }
+                readyQueue.add(process);
+            }
+
+            Process currentProcess = readyQueue.remove();
+            int runTime = currentProcess.getBurstTime();
+            int remainingTime = currentProcess.getRemainingTime();
+
+            if (runTime < testCaseTime) {
+                System.out.print("At time " + now + ": ");
+                CPU.run(currentProcess, runTime);
+                now += runTime;
+                currentProcess.setRemainingTime(0);
+                currentProcess.setFinishTime(now);
+                while(!processesToArrive.isEmpty() &&
+                        processesToArrive.peek().getArrivalTime()<=now){
+                    readyQueue.add(processesToArrive.remove());
+                }
+            }
+
+            else if (remainingTime > testCaseTime) {
+                System.out.print("At time " + now + ": ");
+                CPU.run(currentProcess, testCaseTime);
+                now += testCaseTime ;
+                currentProcess.setFinishTime(now);
+                currentProcess.setRemainingTime(remainingTime - testCaseTime);
+                while(!processesToArrive.isEmpty() &&
+                        processesToArrive.peek().getArrivalTime()<=now){
+                    readyQueue.add(processesToArrive.remove());
+                }
+                readyQueue.add(currentProcess);
+            }
+        }
+    }
 }
